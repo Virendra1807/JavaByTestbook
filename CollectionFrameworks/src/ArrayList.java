961dfde17316c2baa041d3f355421a2d235b6b81
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-08-25 19:51:37 +0530

    HashMap stores key-value pair

---
+++ CollectionFrameworks/src/ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ArrayList {
+    
+    public static void main(String[] args) {
+        
+        List<String> arrList = ArrayList<>();
+
+        arrList.add("January");
+
+
+    }
+}
